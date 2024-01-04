d278bda0dd031e94bba05f5872a3b30c28abafe7
itisameerkhan <itisameerkhan@gmail.com>
2024-01-04 10:40:34 +0530

    login page validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,52 @@
 import './Login.scss';
 import Header from '../Header/Header';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { checkValidData } from '../../utils/validate';
 
 const Login = () => {
 
     const [signUp, setSignUp] = useState(true);
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const username = useRef(null);
+    const email = useRef(null);
+    const password = useRef(null);
 
     const handleClick = () => {
         setSignUp(!signUp);
+    }
+
+    const handleButtonClick = () => {
+        console.log(email);
+        console.log(password);
+        // checkValidData();
     }
 
     return (
         <div className="login">
             <Header />
             <div className="login-form-div">
-                <form className='login-form'>
+                <form className='login-form' onSubmit={(e) => e.preventDefault()}>
                     <label>{signUp ? "Sign In" : "Sign Up"}</label>
-                    <input type="text" placeholder='Username' style={{display: signUp ===true ? 'none' : 'block'}} />
-                    <input type="text" placeholder='Email or phone number' />
-                    <input type="password" placeholder='Password' />
-                    <button className='sign-in-btn'>Sign In</button>
+                    <input 
+                        type="text" 
+                        placeholder='Username' 
+                        style={{display: signUp ===true ? 'none' : 'block'}}
+                        ref={username} 
+                    />
+                    <input type="text" placeholder='Email address' ref={email} />
+                    <div className="password-input">
+                        <input type={passwordVisible ? 'text' : 'password'} placeholder='Password' ref={password} className='password-int' />
+                        <span
+                            onClick={() => setPasswordVisible(!passwordVisible)}>
+                                {passwordVisible ? "HIDE" : "SHOW"}
+                        </span>
+                    </div>
+                    <button 
+                        className='sign-in-btn'
+                        onClick={handleButtonClick}>
+                            {signUp === true ? "Sign In" : "Sign Up"}
+                    </button>
                     <p className='new-to-netflix'>{signUp ? "New to Netflix?" : "Already registered?"} 
                         <span onClick={handleClick}>{signUp ? "Sign up now." : "Sign in now."}</span>
                     </p>
