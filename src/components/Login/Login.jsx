1054ccb0923fddf22e2263697a52fbbaa8254595
itisameerkhan <itisameerkhan@gmail.com>
2024-01-04 09:52:55 +0530

    sign in/up form & login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,28 @@
 import './Login.scss';
 import Header from '../Header/Header';
-import { TextField } from '@mui/material';
+import { useState } from 'react';
 
 const Login = () => {
+
+    const [signUp, setSignUp] = useState(true);
+
+    const handleClick = () => {
+        setSignUp(!signUp);
+    }
+
     return (
-        
         <div className="login">
             <Header />
             <div className="login-form-div">
                 <form className='login-form'>
-                    <label>Sign In</label>
+                    <label>{signUp ? "Sign In" : "Sign Up"}</label>
+                    <input type="text" placeholder='Username' style={{display: signUp ===true ? 'none' : 'block'}} />
                     <input type="text" placeholder='Email or phone number' />
                     <input type="password" placeholder='Password' />
                     <button className='sign-in-btn'>Sign In</button>
+                    <p className='new-to-netflix'>{signUp ? "New to Netflix?" : "Already registered?"} 
+                        <span onClick={handleClick}>{signUp ? "Sign up now." : "Sign in now."}</span>
+                    </p>
                 </form>
             </div>
         </div>
