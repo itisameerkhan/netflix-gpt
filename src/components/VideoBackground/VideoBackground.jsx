4f99c295b08896a83eeeca67ec5c0b37d5cbf426
itisameerkhan <itisameerkhan@gmail.com>
2024-01-06 21:11:56 +0530

    custom Hook: useMovieTrailer

--- src/components/VideoBackground/VideoBackground.jsx
+++ src/components/VideoBackground/VideoBackground.jsx
@@ -1,26 +1,14 @@
-import { useEffect } from 'react';
-import { API_OPTIONS } from '../../utils/constants';
+import useMovieTrailer from '../../hooks/useMovieTrailer';
 import './VideoBackground.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrailerVideo } from '../../utils/movieSlice';
+import { useSelector } from 'react-redux';
+
 
 const VideoBackground = ({movieId}) => {
 
-    const dispatch = useDispatch();
+    useMovieTrailer(movieId);
     const trailerVideo = useSelector(store => store.movies.trailerVideo);
+    console.log(trailerVideo);
 
-    const getMovieVideos = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/1029575/videos?language=en-US', API_OPTIONS);
-        const json = await data.json();
-        const filterData = json?.results.filter(video => video.type === 'Trailer');
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        dispatch(addTrailerVideo(trailer));
-    }
-
-    useEffect(() => {
-        getMovieVideos();
-    },[])
-    
     return (
         <div className="video-background">
            <iframe 
