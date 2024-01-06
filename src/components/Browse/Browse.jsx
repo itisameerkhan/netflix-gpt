91478a1f5c9a59d8e66b7a7b9a5695b796181595
itisameerkhan <itisameerkhan@gmail.com>
2024-01-06 11:42:14 +0530

    Registered API calls from TMDB website

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { API_OPTIONS } from '../../utils/constants';
 import BrowseHeader from '../BrowseHeader/BrowseHeader';
 import './Browse.scss';
 
 const Browse = () => {
+
+    const getNowPlayingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+        const json = await data.json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    },[])
+
     return (
         <div className="browse">
             <BrowseHeader />
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,9 @@
 export const backgroundImg = "https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGIxODE3Yjc1YmI4NzkxYTY5MzliMDllYjEyZmVlZiIsInN1YiI6IjY1OThkOGFlNmU5MzhhMDI1OWNkNjNjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pzWgj2iizsHf1xD4tNeiFRt_-djfuETfSfVf3zPufHc'
+    }
+};
